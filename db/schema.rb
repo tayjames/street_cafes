a91a2aaf7135f5476306b53b451051861db6fe18
Tay <tay.james.d@gmail.com>
2020-02-04 14:52:28 -0700

    Updates schema to include category attribute to Restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_211705) do
+ActiveRecord::Schema.define(version: 2020_02_04_214504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_211705) do
     t.string "address"
     t.string "post_code"
     t.integer "number_of_chairs"
+    t.string "category"
   end
 
 end
